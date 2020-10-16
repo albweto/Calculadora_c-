4f8409264143a966521d0a4c213bfdd986a13cf4
samuel alberto henriquez avila <samualbwt@gmail.com>
2020-10-16 16:09:25 -0500

    Agregar archivos de proyecto.

---
+++ Calculadora/index.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Calculadora.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Calculadora&nbsp; Basica<br />
+        </div>
+        Numero<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;<br />
+        <br />
+        Numero<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Operaciones<asp:DropDownList ID="DropDownList1" runat="server" Height="18px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem Value="S">Suma</asp:ListItem>
+            <asp:ListItem Value="R">Resta</asp:ListItem>
+            <asp:ListItem Value="M">Multiplicacion</asp:ListItem>
+            <asp:ListItem Value="D">Division</asp:ListItem>
+        </asp:DropDownList>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Calcular" />
+        </p>
+        <p>
+            Resultado: <asp:TextBox ID="TextBox3" runat="server" ReadOnly="True"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Calculadora/index.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculadora
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+           
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string numero1 = TextBox1.Text;
+            double numero_1 = double.Parse(numero1);
+            string numero2 = TextBox2.Text;
+            double numero_2 = double.Parse(numero2);
+            double operaciones;
+
+            string operacion = DropDownList1.SelectedValue;
+
+            switch (operacion) {
+                case "S":
+                    operaciones = numero_1 + numero_2;
+                    TextBox3.Text = operaciones.ToString();
+                    break;
+                case "R":
+                    operaciones = numero_1 - numero_2;
+                    TextBox3.Text = operaciones.ToString();
+                    break;
+                case "M":
+                    operaciones = numero_1 * numero_2;
+                    TextBox3.Text = operaciones.ToString();
+                    break;
+                case "D":
+                    if (numero_1 ==0  || numero_2 ==0 ) {
+                        TextBox3.Text = "Error no se puede divir entre cero";
+                    }else{
+                        operaciones = numero_1 / numero_2;
+                        TextBox3.Text = operaciones.ToString();
+                    }
+                   
+                    break;
+            }
+
+        }
+    }
+}
